9e605d3e141fee93fad4ed1069678c011caaca09
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2024-12-07 12:21:23 +0100

    In exercise 113 in folder reactivegui02 in class ConcurrentGUI created inner class Agent with constructor, methods and fields

--- 113-reactive-guis/src/main/java/it/unibo/oop/reactivegui02/ConcurrentGUI.java
+++ 113-reactive-guis/src/main/java/it/unibo/oop/reactivegui02/ConcurrentGUI.java
@@ -2,6 +2,7 @@ package it.unibo.oop.reactivegui02;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -35,5 +36,30 @@ public final class ConcurrentGUI extends JFrame {
         panel.add(stop);
         this.getContentPane().add(panel);
         this.setVisible(true);
+
+        final Agent agent = new Agent();
+        new Thread(agent).start();
+
+        stop.addActionListener((e) -> agent.stopCounting());
+    }
+
+    private class Agent implements Runnable {
+
+        private volatile boolean stop;
+        private volatile boolean inc;
+        private int count;
+
+        @Override
+        public void run() {
+            while(!this.stop) {
+                Random random = new Random();
+                System.out.println(random);
+            }
+        }
+
+        private void stopCounting() {
+            this.stop = true;
+        }
+        
     }
 }
